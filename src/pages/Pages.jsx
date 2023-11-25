345233e88ce85f6f674591838785947d613436eb
divyam <divyamchaudhary112@gmail.com>
2023-11-25 11:35:44 +0530

    first commit

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Home from './home';
+import Cuisine from './Cuisine';
+import Searched from './Searched';
+import Recipe from './Recipe';
+import {Route, Routes} from "react-router-dom" ;
+function pages() {
+  return(
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/cuisine/:type" element={<Cuisine/>}/>
+        <Route path="/searched/:search" element={<Searched/>}/>
+        <Route path="/recipe/:name" element={<Recipe/>}/>
+    </Routes>
+  )
+}
+
+export default pages
